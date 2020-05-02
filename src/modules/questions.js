2e08beb3464cdd3356f4cd44b87a99568f880a31
freedom.yi <851989962freedom.yi@gmail.com>
2020-05-02 15:42:56 +0800

    添加pm2配置文件

--- src/modules/questions.js
+++ src/modules/questions.js
@@ -3,18 +3,18 @@ const { Schema, model } = mongoose;
 
 // 创建一个 用户的 Schema;
 const questionSchema = new Schema({
-  __v: { type: Number, select: false },
-  // 问题名称
-  title: { type: String, required: true },
-  // 问题描述
-  description: { type: String },
-  // 提问者
-  questioner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    select: false,
-  },
+    __v: { type: Number, select: false },
+    // 问题名称
+    title: { type: String, required: true },
+    // 问题描述
+    description: { type: String },
+    // 提问者
+    questioner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        select: false,
+    },
 });
 
 module.exports = model('Question', questionSchema);
